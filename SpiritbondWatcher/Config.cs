c4820b714459794db5fd5512168951642f3f7c5e
xPumaa <35280763+xPumaa@users.noreply.github.com>
2022-03-19 21:18:10 +0100

    Add ConfigUI

---
+++ SpiritbondWatcher/Config.cs
@@ -0,0 +1,26 @@
+﻿namespace SpiritbondWatcher;
+
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+
+[Serializable]
+public class Config : IPluginConfiguration
+{
+    public int Version { get; set; } = 0;
+
+    public bool BondedGearDisplayLineByLine { get; set; } = false;
+
+    [NonSerialized]
+    private DalamudPluginInterface? pluginInterface;
+
+    public void Initialize(DalamudPluginInterface pluginInterface)
+    {
+        this.pluginInterface = pluginInterface;
+    }
+
+    public void Save()
+    {
+        this.pluginInterface!.SavePluginConfig(this);
+    }
+}
